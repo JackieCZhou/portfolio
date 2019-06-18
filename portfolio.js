2f772c2038e56006692155aa1ee929b507a1e55f
Jackie Zhou <Jacqueline.colleen88@gmail.com>
2019-06-18 14:17:20 -0400

    sizing images, addind animation and sidenav

---
+++ portfolio.js
@@ -0,0 +1,11 @@
+
+    document.addEventListener('DOMContentLoaded', function () {
+      var elems = document.querySelectorAll('.sidenav');
+      var instances = M.Sidenav.init(elems, options);
+    });
+
+    document.addEventListener('DOMContentLoaded', function () {
+      var elems = document.querySelectorAll('.tooltipped');
+      var instances = M.Tooltip.init(elems, options);
+    });
+
